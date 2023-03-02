cf4a67493c91f437049a2ac236a50e91d87d5e15
aaronangxz <angxze@gmail.com>
2023-03-03 04:11:39 +0800

    track_checkout implementation

--- impl/tracking/track_checkout/track_checkout.go
+++ impl/tracking/track_checkout/track_checkout.go
@@ -1,1 +1,109 @@
 package track_checkout
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/aaronangxz/AffiliateManager/logger"
+	"github.com/aaronangxz/AffiliateManager/orm"
+	pb "github.com/aaronangxz/AffiliateManager/proto/affiliate"
+	"github.com/aaronangxz/AffiliateManager/resp"
+	"github.com/labstack/echo/v4"
+	"google.golang.org/protobuf/proto"
+	"time"
+)
+
+type TrackCheckOut struct {
+	c   echo.Context
+	ctx context.Context
+	req *pb.TrackCheckOutRequest
+}
+
+func New(c echo.Context) *TrackCheckOut {
+	t := new(TrackCheckOut)
+	t.c = c
+	t.ctx = logger.NewCtx(t.c)
+	logger.Info(t.ctx, "TrackCheckOut Initialized")
+	return t
+}
+
+func (t *TrackCheckOut) TrackCheckOutImpl() *resp.Error {
+	if err := t.verifyTrackCheckOut(); err != nil {
+		return resp.BuildError(err, pb.GlobalErrorCode_ERROR_INVALID_PARAMS)
+	}
+	if err := t.startCheckOutTx(); err != nil {
+		return resp.BuildError(err, pb.GlobalErrorCode_ERROR_FAIL)
+	}
+	return nil
+}
+
+func (t *TrackCheckOut) verifyTrackCheckOut() error {
+	t.req = new(pb.TrackCheckOutRequest)
+	if err := t.c.Bind(t.req); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TrackCheckOut) startCheckOutTx() error {
+	tx := orm.DbInstance(t.ctx).Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			logger.ErrorMsg(t.ctx, "failed to recover: startCheckOutTx")
+		}
+	}()
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	type Booking struct {
+		BookingId          *int64 `gorm:"primary_key"`
+		BookingStatus      *int64
+		BookingDay         *string
+		BookingSlot        *int64
+		TransactionTime    *int64
+		PaymentStatus      *int64
+		CitizenTicketCount *int64
+		TouristTicketCount *int64
+		CitizenTicketTotal *int64
+		TouristTicketTotal *int64
+		CustomerInfo       []byte
+	}
+
+	marshaledInfo, jErr := json.Marshal(t.req.GetCustomerInfo())
+	if jErr != nil {
+		logger.Warn(t.ctx, jErr.Error())
+	}
+
+	b := Booking{
+		BookingId:          nil,
+		BookingStatus:      proto.Int64(int64(pb.BookingStatus_BOOKING_STATUS_SUCCESS)),
+		BookingDay:         t.req.BookingDay,
+		BookingSlot:        t.req.BookingSlot,
+		TransactionTime:    proto.Int64(time.Now().Unix()),
+		PaymentStatus:      proto.Int64(int64(pb.PaymentStatus_PAYMENT_STATUS_SUCCESS)),
+		CitizenTicketCount: t.req.CitizenTicketCount,
+		TouristTicketCount: t.req.TouristTicketCount,
+		CitizenTicketTotal: nil,
+		TouristTicketTotal: nil,
+		CustomerInfo:       marshaledInfo,
+	}
+
+	//Insert into booking_table
+	if err := tx.Create(&b).Error; err != nil {
+		logger.Warn(t.ctx, "Error during startCheckOutTx:updateWalletTransaction: %v", err.Error())
+		tx.Rollback()
+		return err
+	}
+	//Update referral_table using referral_id
+	if err := tx.Raw("UPDATE %v SET WHERE referral_id = %v", orm.REFERRAL_TABLE).Error; err != nil {
+		logger.Warn(t.ctx, "Error during startCheckOutTx:updateWalletTransaction: %v", err.Error())
+		tx.Rollback()
+		return err
+	}
+	logger.Info(t.ctx, "committing startCheckOutTx")
+	if err := tx.Commit().Error; err != nil {
+		return err
+	}
+	return nil
+}
